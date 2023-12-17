a3087accd68f5a1263722167f59a1193aa7dd001
Shreyas <prabhushreyas1@gmail.com>
2023-12-17 23:17:38 +0530

    initialized db and installed gin and gorm

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Shreyas-Prabhu/GoCRUD-1.git/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
